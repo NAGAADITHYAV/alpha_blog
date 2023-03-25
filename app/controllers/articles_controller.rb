6f7b0ba3eace71f6b3c87950068aa7b75508994d
NAGA ADITHYA <nagaadithyavuppalapati@gmail.com>
2023-03-25 22:20:40 +0530

    create and new routes and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article has been created sucessfully'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
