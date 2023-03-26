320c4f70f126063aa61fd1795b2f16cf052a635d
NAGA ADITHYA <nagaadithyavuppalapati@gmail.com>
2023-03-26 16:49:38 +0530

    assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# User Management
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 105 }, 
+                    format: { with: REGEX }
+  has_many :articles
 end
