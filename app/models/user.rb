bc9141a0186c8f994cd35574fe42e019c03a7d17
NAGA ADITHYA <nagaadithyavuppalapati@gmail.com>
2023-03-26 17:05:42 +0530

    password setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 # User Management
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 105 }, 
                     format: { with: REGEX }
+  has_secure_password
+
   has_many :articles
+
+  before_save { self.email = email.downcase }
 end
